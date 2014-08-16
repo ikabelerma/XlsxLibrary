cda9f4c0f73357806221e36194b767cfd80ac6bd
Ika Belerma <ika.belerma@gmail.com>
2014-08-16 17:20:33 +0800

    I can now read and return the value of a cell!
    Either by active sheet or a sheet name. I just wonder if
    I can return non-strings?
    
    Signed-off-by: Ika Belerma <ika.belerma@gmail.com>

--- ExcelXLibrary.py
+++ ExcelXLibrary.py
@@ -1,3 +1,5 @@
+from openpyxl import load_workbook
+
 __author__ = 'ikabelerma'
 
 
@@ -5,5 +7,16 @@ class ExcelXLibrary:
 
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
-    def my_keyword(self, arg):
-        return arg
+    def open_excel_file(self, path):
+        workbook = load_workbook(path)
+        return workbook
+
+    def get_cell_value_by_sheet_name(self, workbook, sheet_name, cell_name):
+        workbook_sheet = workbook.get_sheet_by_name(sheet_name)
+        cell_value = workbook_sheet.cell(cell_name).value
+        return cell_value
+
+    def get_cell_value_of_active_sheet(self, workbook, cell_name):
+        workbook_sheet = workbook.get_active_sheet()
+        cell_value = workbook_sheet.cell(cell_name).value
+        return cell_value
